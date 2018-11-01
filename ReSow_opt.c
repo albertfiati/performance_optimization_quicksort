c53e80add7a18102bfcd85b9f11321423ecc13bb
Albert Fiati-Kumasenu <awkfiati@gmail.com>
2018-11-01 12:07:23 +0100

    Set up for profilling

--- ReSow_opt.c
+++ ReSow_opt.c
@@ -3,11 +3,13 @@
 #include <time.h>
 #include <string.h>
 #include <unistd.h>
+//#include <omp.h>
 
 #define SOURCE_FILE "sourceList.data"
 #define DESTINATION_FILE "sortedList.data"
 #define WRITE "w"
 #define READ "r"
+#define APPEND "a"
 #define QUICKSORT 1
 #define INSERTIONSORT 2
 #define GENERATE 1
@@ -16,18 +18,22 @@
 void loadDataFromFileToMemory(float *buffer, char *filename, int datasetSize);
 int writeDatasetToFile(int datasetSize, float *dataset, char *filename);
 void generateDataset(float *dataset, int datasetSize, int maxNumber);
+void quickSort(float *dataset, int low, int high);
 void insertionSort(float *dataset, int noOfItems);
+int partition(float *dataset, int low, int high);
 float getMinValue(float *dataset, int size);
 float getMaxValue(float *dataset, int size);
 float getAverage(float *dataset, int size);
 void readOptions(int *argc, char **argv);
 void printlist(float *dataset);
+void swap(float *a, float *b);
 void printHelpText();
 void blankline();
 
 int DATA_SOURCE = 1;
 int SIZE = 1024000;
 int PRINT_LIST = 0;
+int ALG = 2;
 int i;
 
 int main(int argc, char *argv[]) {
@@ -54,12 +60,19 @@ int main(int argc, char *argv[]) {
 	printf("** Maximum value is \t%lf\n", getMaxValue(dataset, SIZE));
 
 	blankline();
-	printf(">* Performing insertion sort\n");
-	insertionSort(dataset, SIZE);
+	
+	if(ALG == QUICKSORT){
+		printf(">* Performing quick sort\n");
+		quickSort(dataset, 0, SIZE-1);
+	}else{
+		printf(">* Performing insertion sort\n");
+		insertionSort(dataset, SIZE);
+	}
+
 	blankline();
 	
 	printlist(dataset);
-	
+
 	printf(">* Writing back dataset to file\n");
 	writeDatasetToFile(SIZE, dataset, DESTINATION_FILE);
 
@@ -70,13 +83,16 @@ int main(int argc, char *argv[]) {
 void readOptions(int *argc, char **argv){
 	int option;
 
-	while ((option = getopt (*argc, argv, "hn:d:p:")) != -1){
+	while ((option = getopt (*argc, argv, "hn:a:d:p:")) != -1){
 		switch(option){
 			case 'h':
 				printHelpText();
 				break;
 			case 'n':
 				SIZE = atoi(optarg);
+				break;
+			case 'a':
+				ALG = atoi(optarg);
 				break;
 			case 'd':
 				DATA_SOURCE = atoi(optarg);
@@ -96,8 +112,8 @@ void printHelpText(){
 	printf("  -h 			 Get instructions.\n");
 	printf("  -d [ 1 | 2 ]	\t Set data source for the operation. Options include [1 = generate new dataset, 2 = load dataset from file]. Default is 1\n");
 	printf("  -p [ 1 | 2 ]	\t Prints the list. Options include [1 = print list, 2 = do not print list]. Default is 1\n");
+	printf("  -a [ 1 | 2 ]	\t Define the sorting algorithm to use. Options include [1 = QUICKSORT, 2 = INSERTIONSORT]. Default is 2\n");
 	printf("  -n <size>		 Indicates the size of the dataset to be used. Default is 102400\n");
-	printf("  -b <size>		 Indicates the size of the buffer to be used. Default is 128\n");
 	printf("\n");
 	exit(3);
 }
@@ -175,10 +191,43 @@ void loadDataFromFileToMemory(float *dataset, char *filename, int datasetSize){
 		printf("\nFailed to open file: %s\n", filename);
 		exit(0);
 	}
-	
+
 	fseek(fp, 0, SEEK_SET);
 	fread(dataset, sizeof(float), datasetSize, fp);
 	fclose(fp);
+}
+
+void swap(float *a, float *b)
+{
+	float temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int partition(float *dataset, int low, int high){
+	int pivot = dataset[high];
+	int j;
+
+	i = (low-1);
+
+	//#pragma omp parallel for
+	for(j = low; j <= high-1; j++){
+		if(dataset[j] <= pivot){
+			i++;
+			swap(&dataset[i],&dataset[j]);
+		}
+	}
+
+	swap(&dataset[i + 1],&dataset[high]);
+	return (i + 1);
+}
+
+void quickSort(float *dataset, int low, int high){
+	if(low < high){
+		int pivot = partition(dataset, low, high);
+		quickSort(dataset, low, pivot-1);
+		quickSort(dataset, pivot+1, high);
+	}
 }
 
 void insertionSort(float *dataset, int noOfItems){
