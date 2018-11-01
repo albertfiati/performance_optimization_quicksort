5e6da70c28f62fc20dc76a3d93ca64f4420631a2
Albert Fiati-Kumasenu <awkfiati@gmail.com>
2018-11-01 14:21:31 +0100

    Added chart.py

--- charts.py
+++ charts.py
@@ -1,10 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np 
+
 def main():
+	#scenario1()
+	#scenario2()
+	optimizations()
 
 # solver for question 1
 def scenario1():
 	print 'Scenario 1'
-	
-	performComputations()
 	
 	x = np.array([1024, 10240, 102400, 1024000, 2000000])
 	y = np.array([0, 0, 0.01, 0.34, 0.75])
@@ -12,7 +16,7 @@ def scenario1():
 	plt.plot(x, y, linewidth=2)
 
 	plt.xlabel('Size of dataset')
-	plt.ylabel('Response time')
+	plt.ylabel('Response time ( seconds )')
 	plt.grid(True)
 	plt.legend()
 	plt.show()
@@ -20,18 +24,31 @@ def scenario1():
 def scenario2():
 	print 'Scenario 2'
 	
-	performComputations()
-	
 	x = np.array([1024, 10240, 102400, 1024000, 2000000])
 	y = np.array([0, 0, 0.03, 0.35, 0.80])
 
 	plt.plot(x, y, linewidth=2)
 
 	plt.xlabel('Size of dataset')
-	plt.ylabel('Response time')
+	plt.ylabel('Response time ( seconds )')
 	plt.grid(True)
 	plt.legend()
 	plt.show()
+
+def optimizations():
+	print 'Perfoemance optimizations'
+	
+	objects = ('Insertion Sort', 'Quick Sort', 'Quick sort with OpenMP')
+	y_pos = np.arange(len(objects))
+	performance = [6.98, 0.11, 0.06]
+	 
+	plt.bar(y_pos, performance, align='center', alpha=0.5)
+	plt.xticks(y_pos, objects)
+	plt.ylabel('Response time ( seconds )')
+	plt.xlabel('Algorithms')
+	 
+	plt.show()
+
 
 if __name__ == '__main__':
 	main()	
